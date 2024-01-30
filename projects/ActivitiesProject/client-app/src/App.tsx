62ca1d932248472f3666193ac06513e7bb7a6135
Enveed <sopagnarithseam@gmail.com>
2024-01-30 23:06:20 +0700

    feat - add axios, chakra-ui and react icons packages

--- projects/ActivitiesProject/client-app/src/App.tsx
+++ projects/ActivitiesProject/client-app/src/App.tsx
@@ -1,10 +1,32 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Heading, Icon } from "@chakra-ui/react";
+import { PiUsersThree } from "react-icons/pi";
+import { List, ListItem } from "@chakra-ui/react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/activities")
+      .then((response) => setActivities(response.data));
+  }, []);
 
   return (
-  <h1>Activities</h1>
-  )
+    <div>
+      <Heading as="h2">
+        <Icon as={PiUsersThree} />
+        I'm a Heading
+      </Heading>
+      <List>
+        {activities.map((activity: any) => (
+          <ListItem>{activity.title}</ListItem>
+        ))}
+      </List>
+    </div>
+  );
 }
 
-export default App
+export default App;
