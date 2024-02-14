ac602f12c329a3cad8d6020056b4a6bf147fd674
Enveed <sopagnarithseam@gmail.com>
2024-02-15 00:03:32 +0700

    feat - add semantic-ui to follow course content for frontend faster

--- projects/ActivitiesProject/client-app/src/index.tsx
+++ projects/ActivitiesProject/client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navigation from "./navigation";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider>
